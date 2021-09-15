dbcd0e276652d78b71b95c39ded3b0fca417e1d1
akirasupr <vishal.rockstar7011@gmail.com>
2021-09-16 04:32:00 +0530

    termux setup

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-<h2>Hi, there! I'm Akira! <img src="https://github.githubassets.com/images/mona-whisper.gif" height="24" /></h2>
+This is a simple, personal bootstrap script for [Termux](https://github.com/termux/termux-app/releases) to switch away from the boring BASH shell it comes with as the default.
 
-<p align="center">
-<a href="https://mobile.twitter.com/akirasupr"> <img src="https://img.shields.io/badge/-Twitter-00acee?style=flat&logo=Twitter&logoColor=white" /></a>
-<a href="mailto:akirasupr7011@gmail.com"> <img src="https://img.shields.io/badge/-Gmail-c14438?style=flat&logo=Gmail&logoColor=white"  /></a>
-<a href="https://www.instagram.com/akirasupr"> <img src="https://img.shields.io/badge/-Instagram-c13584?style=flat&labelColor=c13584&logo=instagram&logoColor=white" /></p></a>
+## Requirements:
+ - A device running Android 5.0 or above. (Recommended to have Android 7.0+ coz of [this](https://www.reddit.com/r/termux/comments/dnzdbs/end_of_android56_support_on_20200101/))
+ - Termux app, __duh__. (Install from [F-Droid](https://f-droid.org/packages/com.termux/))
 
-##### I’m currently working on [Nexus Kernel](https://t.me/MercenaryKernel) ;-;
+## Install:
+Installing this is as easy as running the command below in Termux.
+```shell
+bash -c "$(curl -fsSL https://git.io/JEAWN)"
+```
 
-![akira's github stats](https://github-readme-stats.vercel.app/api?username=akirasupr&bg_color=000&show_icons=true&count_private=true&hide_border=true&text_color=2aa889&title_color=ff0000&icon_color=61dafb&include_all_commits=true)
-<p align="center"><a href="https://t.me/NexusKernel"><img alt="Nexus Kernel | Updates" src="https://img.shields.io/badge/dynamic/json?logo=telegram&label=%40NexusKernel&labelColor=282c34&suffix=+members&color=eb0029&query=%24.data.totalSubs&url=https%3A%2F%2Fapi.spencerwoo.com%2Fsubstats%2F%3Fsource%3Dtelegram%26queryKey%3DNexusKernel&longCache=true"/></a>
+Don't worry, the existing `setup` will be backed up in your storage for later. You can find it in the `backup` directory at the location below.
+> Internal Storage (`/sdcard` [or] `/storage/emulated/0`) -> `setup`
---
+++ setup.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+# Clear the screen
+clear
+
+# Turn off cursor.
+setterm -cursor off
+
+# Get fastest mirrors.
+echo -n -e "Syncing with fastest mirrors. \033[0K\r"
+pkg update -y 2>/dev/null
+
+# Upgrade packages.
+echo -n -e "Upgrading packages. \033[0K\r"
+apt-get upgrade -o Dpkg::Options::='--force-confnew' -y 2>/dev/null
+
+# Updating package repositories and installing packages.
+echo -n -e "Installing required packages. \033[0K\r"
+apt update 2>/dev/null
+apt install -y curl git wget shc aria2 gh 2>/dev/null
+sleep 2
+
+# Giving Storage permision to Termux App.
+if [ ! -d $HOME/storage ]; then
+    echo -n -e "Setting up storage access for Termux. \033[0K\r"
+    termux-setup-storage
+    sleep 2
+fi
+
+# Backing up your setup
+mkdir -p $HOME/storage/shared/setup
+for i in "$HOME/.termux/font.ttf" "$HOME/.termux/colors.properties" "$HOME/.termux/termux.properties"
+do
+    if [ -f $i ]; then
+        echo -n -e "Backing up current $i file. \033[0K\r"
+        mv -f $i $HOME/storage/shared/setup/$(date +%Y_%m_%d_%H_%M)/$(basename $i)
+        sleep 1
+    fi
+done
+sleep 2
+
+# Upgrade packages.
+echo -n -e "Updating system packages. \033[0K\r"
+pkg upgrade -y 2>/dev/null
+sleep 2
+
+# Installing the Ubuntu font for Termux.
+if [ ! -f $HOME/.termux/font.ttf ]; then
+    echo -n -e "Installing Ubuntu font. \033[0K\r"
+    curl -fsSL -o $HOME/.termux/font.ttf 'https://raw.githubusercontent.com/akirasupr/akirasupr/setup/font.ttf'
+    sleep 2
+fi
+
+# Set a default color scheme.
+echo -n -e "Setting up a new color scheme. \033[0K\r"
+curl -fsSL -o $HOME/.termux/colors.properties 'https://raw.githubusercontent.com/akirasupr/akirasupr/setup/colors.prop'
+sleep 2
+
+# Add new buttons to the Termux bottom bar.
+echo -n -e "Setting up some extra keys in Termux. \033[0K\r"
+curl -fsSL -o $HOME/.termux/termux.properties 'https://raw.githubusercontent.com/akirasupr/akirasupr/setup/termux.prop'
+sleep 2
+
+# Setup git credentials
+echo -n -e "Setting up Git Credentials in Termux. \033[0K\r"
+if [ ! -f $HOME/.gitconfig ]; then
+    touch $HOME/.gitconfig && clear
+    if [ ! $(grep "name" $HOME/.gitconfig) ]; then
+        echo -n "Please enter your user name: "
+        read -r name
+        git config --global user.name "${name}"
+    fi
+    if [ ! $(grep "email" $HOME/.gitconfig) ]; then
+        echo -n "Please enter your email id: "
+        read -r email
+        git config --global user.email "${email}"
+    fi
+    if [ ! $(grep "editor" $HOME/.gitconfig) ]; then
+        echo -n "Please enter your core editor name: "
+        read -r editor
+        git config --global core.editor "${editor}"
+    fi
+    clear
+fi
+echo -n -e "Successfully updated Git Credentials \033[0K\r"
+sleep 2
+
+# Setup Gh
+echo -n -e "Setting up Gh in Termux. \033[0K\r"
+if [ ! $HOME/.config/gh ]; then
+    gh auth login
+    clear
+fi
+echo -n -e "Successfully updated Gh \033[0K\r"
+sleep 1
+
+# Reload Termux settings.
+termux-reload-settings
+
+# Setup complete.
+echo -n -e "Installation complete! \033[0K\r"
+sleep 1
+
+# Restore cursor.
+setterm -cursor on
+
+# Clear the screen
+clear
+
+# Setup finished
+exit
